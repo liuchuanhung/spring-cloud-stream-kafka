35da904f738229caef359505a3772c3f15691295
Ryan Liu <ryan.liu@slalom.com>
2020-09-21 15:51:32 -0500

    initial commit.

---
+++ src/main/java/com/liuchuanhung/spring/cloud/stream/publisher/dto/Book.java
@@ -0,0 +1,13 @@
+package com.liuchuanhung.spring.cloud.stream.publisher.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    private int id;
+    private String name;
+}
